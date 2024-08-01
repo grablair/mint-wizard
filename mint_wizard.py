6d99d6121c28183989c6a7444b37b48602aae42f
Graham Blair <graham@grahamarthurblair.com>
2024-07-31 18:52:28 -0700

    Ironically remove Mint-related logic

--- mint_wizard.py
+++ mint_wizard.py
@@ -46,7 +46,7 @@ def add_recurring_txn(args):
 	elif args.category in shorthands.keys():
 		category = shorthands[args.category]
 	else:
-		logger.error(f"ERROR: Given category {args.category} not a valid shorthand or Mint category.")
+		logger.error(f"ERROR: Given category {args.category} not a valid shorthand or category.")
 		sys.exit(1)
 
 	if args.move_from_category:
@@ -71,77 +71,47 @@ def remove_recurring_txn(args):
 	args.db.remove_recurring_transaction(args.id)
 
 def run_auto_processor(args):
-	logger.info("Starting run of the Splitwise Budgeting Auto-Processor")
+	logger.info("Starting run of the Budgeting Auto-Processor")
 
 	creds = json.load(open(args.credentials_path))
 	config = json.load(open(args.config))
 
-	if args.use_monarch_money:
-		mm = MonarchMoneyHelper(creds, args.db, args.mm_session_pickle_file)
+	mm = MonarchMoneyHelper(creds, args.db, args.mm_session_pickle_file)
 
-		if args.splitwise:
-			splitwise = SplitwiseHelper(creds, mm, args.shorthand_json_path, args.splitwise_user_id_to_name_json, args.custom_user_identifier, args.db)
+	if args.splitwise:
+		splitwise = SplitwiseHelper(creds, mm, args.shorthand_json_path, args.splitwise_user_id_to_name_json, args.custom_user_identifier, args.db)
 
-			# Process Splitwise expenses and add transactions to Monarch Money
-			splitwise.process_splitwise_expenses(args.splitwise_days_to_look_back)
+		# Process Splitwise expenses and add transactions to Monarch Money
+		splitwise.process_splitwise_expenses(args.splitwise_days_to_look_back)
 
-			if 'payment_import_rules' in config:
-				splitwise.import_payments_from_budgeting_app(config['payment_import_rules'])
+		if 'payment_import_rules' in config:
+			splitwise.import_payments_from_budgeting_app(config['payment_import_rules'])
 
-			if 'loans' in config:
-				splitwise.handle_personal_loans(config['loans'])
-		else:
-			logger.info("Skipping Splitwise processing, as instructed")
-
-		if args.recategorize_txns and "patterns_to_recategorize" in config:
-			mm.recategorize_target_transactions(config["patterns_to_recategorize"])
-
-		if args.recurring_txns:
-			# Add any recurring transactions to Monarch Money
-			mm.process_recurring_transactions()
-
-		if "account_balance_export_webhook" in config :
-			# Export account balances to the given webhook
-			mm.export_account_balances(config["account_balance_export_webhook"])
-
-		if "auto_splits" in config:
-			# Process auto-splits
-			mm.handle_auto_splits(config["auto_splits"])
-
-		if "account_growth_partners" in config:
-			# Sync account growth between partner accounts
-			mm.sync_account_growth(config["account_growth_partners"])
-
-		logger.info("Splitwise-Budgeting auto-processing via Monarch Money complete!")
+		if 'loans' in config:
+			splitwise.handle_personal_loans(config['loans'])
 	else:
-		if (args.remote is None) != (args.remote_url is None):
-			log.error("You must specify both --remote and --remote-url, or neither")
-			sys.exit(1)
+		logger.info("Skipping Splitwise processing, as instructed")
 
-		if args.custom_user_identifier and not re.match(r'^[A-Z]+$', args.custom_user_identifier):
-			print("--mint-custom-user-identifier must be solely positive letters from A-Z. It was: {}".format(args.custom_user_identifier))
-			sys.exit()
-		with MintHelper(creds, args.db, args.headless, args.driver, args.remote, args.remote_url) as mint:
-			mint.load_transactions_page()
+	if args.recategorize_txns and "patterns_to_recategorize" in config:
+		mm.recategorize_target_transactions(config["patterns_to_recategorize"])
 
-			if args.splitwise:
-				splitwise = SplitwiseHelper(creds, mint, args.shorthand_json_path, args.splitwise_user_id_to_name_json, args.custom_user_identifier, args.db)
+	if args.recurring_txns:
+		# Add any recurring transactions to Monarch Money
+		mm.process_recurring_transactions()
 
-				# Process Splitwise expenses and add transactions to Mint
-				splitwise.process_splitwise_expenses(args.splitwise_days_to_look_back)
-			else:
-				logger.info("Skipping Splitwise processing, as instructed")
+	if "account_balance_export_webhook" in config :
+		# Export account balances to the given webhook
+		mm.export_account_balances(config["account_balance_export_webhook"])
 
-			# Recategorize transactions in Mint
-			# TODO: Move to database with multiple patterns (name, price, etc)
-			if args.recategorize_txns and "patterns_to_recategorize" in config:
-				mint.recategorize_target_transactions(config["patterns_to_recategorize"])
+	if "auto_splits" in config:
+		# Process auto-splits
+		mm.handle_auto_splits(config["auto_splits"])
 
-			if args.recurring_txns:
-				# Add any recurring transactions to Mint
-				mint.process_recurring_transactions()
+	if "account_growth_partners" in config:
+		# Sync account growth between partner accounts
+		mm.sync_account_growth(config["account_growth_partners"])
 
-			logger.info("Splitwise-Budgeting auto-processing via Mint complete!")
+	logger.info("Budgeting auto-processing via Monarch Money complete!")
 
 if __name__ == "__main__":
 
@@ -154,24 +124,18 @@ if __name__ == "__main__":
 
 	auto_process_parser = subparsers.add_parser("auto-process", help="Run the auto-processor")
 	auto_process_parser.add_argument("-creds", "--credentials-path", help="The path to the file containing your credentials", required=True)
-	auto_process_parser.add_argument("-short", "--shorthand-json-path", help="The path to the file containing the mapping of shorthand identifiers to mint categories", default=f"{mint_wizard_dir}/shorthands.json")
+	auto_process_parser.add_argument("-short", "--shorthand-json-path", help="The path to the file containing the mapping of shorthand identifiers to categories", default=f"{mint_wizard_dir}/shorthands.json")
 	auto_process_parser.add_argument("-names", "--splitwise-user-id-to-name-json", help="The path of the JSON file used to override names fetched from Splitwise")
 	auto_process_parser.add_argument("-userid", "--custom-user-identifier", help="Turns on user-specific Splitwise flags. See README")
 	auto_process_parser.add_argument("-config", help="Path to config file with recurring transactions and recategorizations", default=f"{mint_wizard_dir}/config.json")
 	auto_process_parser.add_argument("-days", "--splitwise-days-to-look-back", help="The number of days to look back when determining expenses to process (script looks at updated dates, not dates of the expenses)", type=int, default=2)
 	auto_process_parser.add_argument("--recurring-txns", help="Process recurring transactions", action=argparse.BooleanOptionalAction, default=True)
 	auto_process_parser.add_argument("--recategorize-txns", help="Perform transaction recategorization", action=argparse.BooleanOptionalAction, default=True)
-	auto_process_parser.add_argument("--splitwise", help="Process splitwise transactions", action=argparse.BooleanOptionalAction, default=True)
-	
-	auto_process_parser.add_argument("-driver", "--selenium-driver", help="Driver to use for Selenium", choices=["FIREFOX", "CHROME"], default="FIREFOX", dest="driver")
-	auto_process_parser.add_argument("--remote", help="Connect to a remote WebDriver (must also specify --remote-url)", action=argparse.BooleanOptionalAction)
-	auto_process_parser.add_argument("--remote-url", help="Remote URL of your Selenium WebDriver")
-	auto_process_parser.add_argument("--headless", help="Run the Selenium driver in headless mode", action=argparse.BooleanOptionalAction, default=True)
-	auto_process_parser.add_argument("--use-monarch-money", help="Use Monarch Money instead of Mint", action=argparse.BooleanOptionalAction, default=False)
+	auto_process_parser.add_argument("--splitwise", help="Process splitwise transactions", action=argparse.BooleanOptionalAction, default=True)	
 	auto_process_parser.add_argument("--mm-session-pickle-file", help="The file to save cookies and auth tokens to for Monarch Money", default=f"{mint_wizard_dir}/mm_session.pickle")
 	auto_process_parser.set_defaults(func=run_auto_processor)
 
-	recurring_transactions_subparser = subparsers.add_parser("recurring-txns", help="Configure recurring Mint transactions")
+	recurring_transactions_subparser = subparsers.add_parser("recurring-txns", help="Configure recurring transactions")
 	recurring_transactions_subparsers = recurring_transactions_subparser.add_subparsers(required=True)
 
 	recurring_transactions_subparsers.add_parser("list", help="List the configured recurring transactions").set_defaults(func=list_recurring_txns)
@@ -181,8 +145,8 @@ if __name__ == "__main__":
 	add_recurring_txn_parser.add_argument("-a", "--amount", help="Amount of the transaction. Negative numbers are charges, positive numbers are credits", required=True)
 	add_recurring_txn_parser.add_argument("-c", "--category", help="Mint category for the transaction", required=True)
 	add_recurring_txn_parser.add_argument("-r", "--recurrence-rule", help="Natural language representation of a recurring event. Can include start dates and end dates, but they are not required. Ex: \"every 2 weeks starting next monday until jan\", \"every day\"", dest="recurring_event", required=True, type=util.str_to_valid_recurring_event)
-	add_recurring_txn_parser.add_argument("-short", "--shorthand-json-path", help="The path to the file containing the mapping of shorthand identifiers to mint categories", default=f"{mint_wizard_dir}/shorthands.json")
-	add_recurring_txn_parser.add_argument("-mc", "--move-from-category", help="Mint category to move the transaction FROM. This will create two recurring transactions: one credit to the FROM category, and one charge to the -c category")
+	add_recurring_txn_parser.add_argument("-short", "--shorthand-json-path", help="The path to the file containing the mapping of shorthand identifiers to categories", default=f"{mint_wizard_dir}/shorthands.json")
+	add_recurring_txn_parser.add_argument("-mc", "--move-from-category", help="Category to move the transaction FROM. This will create two recurring transactions: one credit to the FROM category, and one charge to the -c category")
 	add_recurring_txn_parser.set_defaults(func=add_recurring_txn)
 
 	remove_recurring_txn_parser = recurring_transactions_subparsers.add_parser("remove", help="Remove a recurring transaction")
